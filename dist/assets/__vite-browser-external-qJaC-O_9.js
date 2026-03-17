e1c0cd5b9145157bb96111e782248158dab6cf12
stagas <gstagas@gmail.com>
2026-03-17 10:09:27 +0200

    build: update

---
+++ dist/assets/__vite-browser-external-qJaC-O_9.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-BvD6h4u_.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-qJaC-O_9.js.map
