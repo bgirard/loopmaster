70fda15c3d3940a38d9f1b0c638120f3e844154a
stagas <gstagas@gmail.com>
2026-04-16 08:00:45 +0300

    save

---
+++ dist/assets/__vite-browser-external-CnBHdREY.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-C13o6LZg.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-CnBHdREY.js.map
--- dist/assets/__vite-browser-external-Dd9DLkXk.js
+++
@@ -1,7 +0,0 @@
-import { t as __commonJSMin } from "./index-BR8x5UX-.js";
-var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
-	module.exports = {};
-}));
-export default require___vite_browser_external();
-
-//# sourceMappingURL=__vite-browser-external-Dd9DLkXk.js.map
